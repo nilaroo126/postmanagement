da22fe1963ba96169f32962ca496d40b93ebd181
nilaroo <nilaroo.mmhu@gmail.com>
2025-03-15 22:50:40 +0630

    Add More Database

--- src/main/java/com/ecom/shopping_cart/PostManagementApplication.java
+++ src/main/java/com/ecom/shopping_cart/PostManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingCartApplication {
+public class PostManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingCartApplication.class, args);
+        SpringApplication.run(PostManagementApplication.class, args);
     }
 
 }
